fce7a5df03b1698801229741deb9d9541c773959
RiverFreez-Tv <riverfree.off@gmail.com>
2025-10-24 22:17:40 +0200

    Change SN RADIO stream URL
    Updated the radio stream URL to a new address.

--- sn-radio-main/fxmanifest.lua
+++ sn-radio-main/fxmanifest.lua
@@ -6,7 +6,7 @@ description 'Custom Radio'
 version '1.0.0'
 
 -- SN RADIO 
-supersede_radio 'RADIO_02_POP' { url = 'http://31.59.234.111/listen/sn_radio/radio.mp3', volume = 0.5, name = 'SN RADIO' }
+supersede_radio 'RADIO_02_POP' { url = 'http://sn-radio.online/listen/sn_radio/radio.mp3', volume = 0.5, name = 'SN RADIO' }
 
 files {
 	'index.html',
